b0496b5ee419ecca03fcea002964cf79fe29e7f2
Nishita Dalmia <76201488+Nishitadalmia@users.noreply.github.com>
2025-03-16 22:05:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ authenticator = stauth.Authenticate(
     config['cookie']['expiry_days']
 )
 
-name, authentication_status, username = authenticator.login('Login', 'main')
+name, authentication_status, username = authenticator.login('Login', 'sidebar')
 
 # ------ DATABASE SETUP ------ #
 conn = sqlite3.connect("tasks_notes.db")
